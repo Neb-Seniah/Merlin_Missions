db2050c69532c93f444a226ee28f37bb2e18b084
benjamin-haines <Benjamin Haines>
2022-11-16 09:25:06 -0600

    Yes

---
+++ M2222-10 (01) Recursion/C101 Factorial [Swift]/main.swift
@@ -0,0 +1,23 @@
+func facts (_ n: Int) -> Int {
+    print("Calculating factorial("+String(n)+")...")
+    if n <= 1 {
+        //Signaling the unraveling of the function
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        //loops the function and causes it to stack
+        let finale = n * facts(n-1)
+        print("Non-terminal case, returning " + String(finale) + ".")
+        return finale
+    }
+}
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+print("The final result is: " + String(facts(n)) + ".")
---
+++ M2222-10 (01) Recursion/C102 Fibonacci [Swift]/.merlin/build/main.swift
@@ -0,0 +1,17 @@
+func fibon (_ n: Int) -> Int {
+    print("Calculating fibonacci(" + String(n) + ")...")  
+    if n <= 2 {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let finale = fibon(n-1) + fibon(n-2)
+        print("Non-terminal case, returning " + String(finale) + ".")
+        return finale
+    }
+}
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+    print("The final result is: " + String(fibon(n)) + ".")
